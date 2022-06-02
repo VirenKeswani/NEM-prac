c6c80c2dbcb1d40211330550036ba5e77b2aecb7
Viren Keswani <virenkeswani@gmail.com>
2022-06-02 18:39:38 +0530

    added basic server hosting

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const express = reqire('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static(__dirname + '/public'))
+
+
+app.listen(process.env.PORT || 3000)
