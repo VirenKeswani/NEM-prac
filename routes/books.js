8ca3922967af2722176a0cc153b83deffe99381b
Viren Keswani <virenkeswani@gmail.com>
2022-06-05 21:11:18 +0530

    added author choosing option in books/new

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+const Author = require('../models/author')
 
 //get all books routes
 router.get('/', async (req, res) => {
     res.render('books/index')
+   //res.send('books')
 })
 
 
 // new book routes
-router.get('/new', (req, res) => {
-    res.render('books/new')
+router.get('/new', async(req, res) => {
+    try{
+        const author= await Author.find({})
+        const book = new Book()
+        res.render('books/new', {
+            author : author,
+            book : book
+        })
+    }
+    catch{
+        res.redirect('/books')
+    }
+    //res.send('new book')
 })
 
 
 //create book
 router.post('/', async (req, res) => {
-    
+    //res.send("Create Book")
 })
 module.exports=router
